5e373c2c216aad74fe39e1f0ae413130706391d3
omolo-j <omoloj135@gmail.com>
2024-07-04 10:55:56 +0300

    cookies-and-sessions-lab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, jsonify, session
 from flask_migrate import Migrate
-
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -12,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
 @app.route('/clear')
@@ -22,13 +20,42 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    articles_data = [
+        {
+            'id': article.id,
+            'title': article.title,
+            'content': article.content,
+            'author': article.author,
+            'preview': article.preview,
+            'minutes_to_read': article.minutes_to_read,
+            'date': article.date.isoformat()  # Convert date to ISO format string
+        }
+        for article in articles
+    ]
+    return jsonify(articles_data), 200
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
 
-    pass
+    if session['page_views'] <= 3:
+        article = db.session.get(Article, id)
+        if article:
+            article_data = {
+                'id': article.id,
+                'title': article.title,
+                'content': article.content,
+                'author': article.author,
+                'preview': article.preview,
+                'minutes_to_read': article.minutes_to_read,
+                'date': article.date.isoformat()  # Convert date to ISO format string
+            }
+            return jsonify(article_data), 200
+        else:
+            return {'message': 'Article not found'}, 404
+    else:
+        return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000)
